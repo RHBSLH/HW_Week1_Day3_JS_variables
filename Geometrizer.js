9eabdf744e95ee7cf2512afc489f10a91dd6eece
RHBSLH <Rehab.Saleh.abt.js@tuwaiq.edu.sa>
2021-10-13 09:37:17 +0300

    add some information in HW W1 D3

--- Geometrizer.js
+++ Geometrizer.js
@@ -1,5 +1,7 @@
 let radius =4
 let circumference =Math.PI * 2 *radius 
-console.log ("The circumference is " +circumference ) 
+
+console.log ("The circumference is " +circumference.toFixed(2) ) 
+// use {circumference.toFixed(2)} to select 2 number after the commah
 let area = Math.PI *radius*radius
-console.log("The area is " + area)
+console.log("The area is " + area.toFixed(2))
